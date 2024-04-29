385efbdab0962add2f3f974bfed9e56ac1a735e4
yuseong <dbtjd7282@gmail.com>
2024-04-29 18:41:45 +0900

    first commit

---
+++ SYS20211231_mid.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class SYS20211231_mid {
+    //타이틀 출력
+    public static void printHead() {
+        System.out.print("""
+            ------------------------------
+                     가위바위보 게임
+            ------------------------------
+            """);
+        System.out.println("0.가위 1.바위 2.보 중 하나 선택해주세요");
+    }
+    //입력값 예외처리 - min, max 범위X and 숫자가 아닐 경우 -1 반환
+    public static int checkUser(String lineinput, int min, int max) {
+        try{
+            int userChoice = Integer.parseInt(lineinput); //int로 변환
+            //범위 지정
+            if (userChoice < min || userChoice > max) {
+                System.out.println("[ERROR] 0, 1, 2 중 하나를 입력해주세요.");
+                return -1;
+            }
+            return userChoice;
+        } //예외처리
+        catch(Exception e){
+            System.out.println("[ERROR] 0, 1, 2 중 하나를 입력해주세요.");
+            return -1;
+        }
+    }
+    //입력받기
+    public static int getInput(int min, int max){
+        Scanner sc = new Scanner(System.in); //Scanner 객체 생성
+        System.out.print("입력 : ");
+        int input = checkUser(sc.nextLine(), min, max);
+        //예외처리 과정 후 -1 반환 시 다시 입력
+        while(input == -1) {
+            System.out.print("다시 입력해주세요 : ");
+            input = checkUser(sc.nextLine(), min, max);
+        }
+        return input;
+    }
+    //승자 결정
+    public static int winnerDetermine(int userChoice){
+        Random r = new Random(); //Random 객체 생성
+        int computer = r.nextInt(3); //computer변수 0,1,2 무작위 선택
+        String [] iswhat = {"가위", "바위", "보"};
+        //wCondition[0] = 가위, wCondition[1] = 바위, wCondition[2] = 보, 그에 따른 승리 조건 = 1.승리 0.무승부 -1.패배
+        int [][] wCondition = {
+                {0,-1,1},{1,0,-1},{-1,1,0}
+        };
+        //누가 무엇을 냈는지
+        System.out.printf("사용자 : %s  |  컴퓨터 : %s\n", iswhat[userChoice], iswhat[computer]);
+        return wCondition[userChoice][computer];
+    }
+    //재시작 여부
+    public static boolean oneMore()
+    {
+        System.out.println("계속 하시겠습니까? 1.계속, 2.종료");
+        int input = getInput(1,2); //1,2만 입력 가능
+        if (input == 1) {
+            return true;
+        }
+        else {
+            System.out.println("종료하겠습니다.");
+            return false;
+        }
+    }
+    //게임 플레이
+    public static void gamePlay()
+    {
+        printHead(); //시작문구 출력
+        int result = winnerDetermine(getInput(0,2));
+        if(result == 1) {
+            System.out.println("승리");
+        }
+        else if (result == -1) {
+            System.out.println("패배");
+        }
+        else {
+            System.out.println("무승부");
+        }
+    }
+    //main
+    public static void main(String[] args) {
+        boolean cont = true; //cont값 초기화
+        while(cont) {
+            gamePlay();
+            cont = oneMore();
+        }
+    }
+}
---
+++ SYS20211231_mid2.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+
+public class SYS20211231_mid2 {
+    //게임 타이틀, 선택지 출력
+    public static void printHead() {
+        System.out.print("""
+                ------------------------------
+                           묵찌빠 게임
+                ------------------------------
+                """);
+        System.out.println("0.가위 1.바위 2.보 중 하나 선택해주세요");
+    }
+    //첫 선공 가위바위보 게임
+    public static int rspGame() {
+        SYS20211231_mid sys = new SYS20211231_mid(); //가위바위보 함수 객체생성
+        int result; // 1 : 승리, 0 : 무승부 -1 : 패배, 확인 변수
+        while (true) {
+            result = sys.winnerDetermine(sys.getInput(0, 2)); //함수를 통해 승리 변수 대입
+            //승리 및 패배 시 반복문 탈출 그러나 무승부 시 가위바위보 재개
+            if (result == 1) {
+                break;
+            } else if (result == -1) {
+                break;
+            } else {
+                System.out.println("무승부");
+                System.out.println("다시 입력해주세요 - 0.가위 1.바위 2.보");
+            }
+        }
+        return result;
+    }
+
+    static void mjpGame() {
+        SYS20211231_mid sys = new SYS20211231_mid();
+        int detatt = rspGame(); //승리변수 대입
+        while (true) {
+            //승리변수를 통해 공격 및 수비 여부 출력
+            if (detatt == 1) {
+                System.out.println("공격입니다.");
+            }
+            else {
+                System.out.println("수비입니다.");
+            }
+            System.out.println("무엇을 내시겠습니까?");
+            int result = sys.winnerDetermine(sys.getInput(0, 2)); //공격수비 묵찌빠 변수 입력
+            //공격 시 사용자와 컴퓨터가 낸 값이 같다면 승리
+            if (detatt == 1) {
+                if (result == 0) {
+                    System.out.println("승리");
+                    break;
+                }
+                detatt = result; //만약 승리하지 못했다면 공격수비 변수 업데이트
+            }
+            //수비 시 사용자와 컴퓨터가 낸 값이 같다면 패배
+            else {
+                if (result == 0) {
+                    System.out.println("패배");
+                    break;
+                }
+                detatt = result; //만약 수비를 잘 했다면 공격수비 변수 업데이트
+            }
+        }
+    }
+
+    public static void gameplay() {
+        printHead();
+        mjpGame();
+    }
+
+    public static void main(String[] args) {
+        boolean cont = true; //cont값 초기화
+        while(cont) {
+            gameplay();
+            cont = SYS20211231_mid.oneMore(); //한번 더?
+        }
+    }
+}
